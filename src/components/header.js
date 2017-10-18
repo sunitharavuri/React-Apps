a3972d211452968c9acbceaef9514c851e71daa8
Nisum <nisum@Admins-MacBook-Pro.local>
2017-10-18 14:57:45 -0700

    adding

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import './App.css';
+
+ export default  class Header extends React.Component{
+    render(){
+        return(
+        <h1>React Todo's</h1>
+  );
+}
+}
